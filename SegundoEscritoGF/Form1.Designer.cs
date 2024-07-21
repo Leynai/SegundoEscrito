6d5639801e37d2333e04b9094c9c3a3d506466c4
Fialho <Guillermoezequielfg@gmail.com>
2024-07-20 21:04:31 -0300

    jeje hice lo visual y un poquis de codigo porque me olvide de usar github, cosas de boludo

--- SegundoEscritoGF/Form1.Designer.cs
+++ SegundoEscritoGF/Form1.Designer.cs
@@ -29,33 +29,127 @@ namespace SegundoEscritoGF
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.LabelNombre = new System.Windows.Forms.Label();
+            this.LabelApellido = new System.Windows.Forms.Label();
+            this.ApellidoTextBox = new System.Windows.Forms.TextBox();
+            this.LabelID = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.GuardarPersonaButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // NombreTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 125);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.NombreTextBox.Location = new System.Drawing.Point(66, 56);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombreTextBox.TabIndex = 0;
+            this.NombreTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // LabelNombre
+            // 
+            this.LabelNombre.AutoSize = true;
+            this.LabelNombre.Location = new System.Drawing.Point(16, 59);
+            this.LabelNombre.Name = "LabelNombre";
+            this.LabelNombre.Size = new System.Drawing.Size(44, 13);
+            this.LabelNombre.TabIndex = 1;
+            this.LabelNombre.Text = "Nombre";
+            // 
+            // LabelApellido
+            // 
+            this.LabelApellido.AutoSize = true;
+            this.LabelApellido.Location = new System.Drawing.Point(16, 102);
+            this.LabelApellido.Name = "LabelApellido";
+            this.LabelApellido.Size = new System.Drawing.Size(44, 13);
+            this.LabelApellido.TabIndex = 2;
+            this.LabelApellido.Text = "Apellido";
+            // 
+            // ApellidoTextBox
+            // 
+            this.ApellidoTextBox.Location = new System.Drawing.Point(66, 99);
+            this.ApellidoTextBox.Name = "ApellidoTextBox";
+            this.ApellidoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ApellidoTextBox.TabIndex = 3;
+            // 
+            // LabelID
+            // 
+            this.LabelID.AutoSize = true;
+            this.LabelID.Location = new System.Drawing.Point(29, 145);
+            this.LabelID.Name = "LabelID";
+            this.LabelID.Size = new System.Drawing.Size(18, 13);
+            this.LabelID.TabIndex = 4;
+            this.LabelID.Text = "ID";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Location = new System.Drawing.Point(66, 142);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.IdTextBox.TabIndex = 5;
+            // 
+            // GuardarPersonaButton
+            // 
+            this.GuardarPersonaButton.Location = new System.Drawing.Point(79, 183);
+            this.GuardarPersonaButton.Name = "GuardarPersonaButton";
+            this.GuardarPersonaButton.Size = new System.Drawing.Size(75, 23);
+            this.GuardarPersonaButton.TabIndex = 6;
+            this.GuardarPersonaButton.Text = "Guardar";
+            this.GuardarPersonaButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(277, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(441, 299);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.NombreTextBox);
+            this.panel1.Controls.Add(this.LabelNombre);
+            this.panel1.Controls.Add(this.LabelApellido);
+            this.panel1.Controls.Add(this.GuardarPersonaButton);
+            this.panel1.Controls.Add(this.ApellidoTextBox);
+            this.panel1.Controls.Add(this.IdTextBox);
+            this.panel1.Controls.Add(this.LabelID);
+            this.panel1.Location = new System.Drawing.Point(27, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 241);
+            this.panel1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(783, 446);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.Label LabelNombre;
+        private System.Windows.Forms.Label LabelApellido;
+        private System.Windows.Forms.TextBox ApellidoTextBox;
+        private System.Windows.Forms.Label LabelID;
+        private System.Windows.Forms.TextBox IdTextBox;
+        private System.Windows.Forms.Button GuardarPersonaButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
